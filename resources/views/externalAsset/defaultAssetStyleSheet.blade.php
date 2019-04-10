11ad45c81ec5f420b330794f1c6771aedea19938
Anthony Derecho <derechoanthony@gmail.com>
2019-04-10 23:40:31 +0800

    user registration

--- resources/views/externalAsset/defaultAssetStyleSheet.blade.php
+++ resources/views/externalAsset/defaultAssetStyleSheet.blade.php
@@ -1,3 +1,7 @@
 <link rel="stylesheet" href="{{ asset('external/default/css/styles-merged.css') }}">
 <link rel="stylesheet" href="{{ asset('external/default/css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('external/default/css/custom.css') }}">
+
+@if (Request::is('register'))  
+    <link href="{{ asset('css/stepper.css') }}" rel="stylesheet" />
+@endif
